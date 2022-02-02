0f569b87df39f099f11e3d683e495c69dd08a237
LoopFruits <Kensworldc@gmail.com>
2022-02-02 16:27:31 -0500

    filled out index.html

---
+++ index.js
@@ -0,0 +1,22 @@
+// Scope 
+
+
+    // Lexical Scope 
+        // Can: Acess variable ion outer scope 
+        // Can't: access variables in nested scope 
+
+
+    // Global Scope
+        // Acessible in every scope
+        // is the Outermost scope
+    
+    //Functional Scope
+        //Acessible within a function
+        //Specific to a single function
+        //Can't be accessed by anything in outer scopes
+
+
+    // Block 
+        //Accessible within a block 
+        //Specific block
+        //Can't be accesed by anything in outer scopes
